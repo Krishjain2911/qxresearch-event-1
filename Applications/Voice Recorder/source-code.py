21382449836c61f30fac9b98dc118168d3c7e197
Krish <Krishjain2911@gmail.com>
2021-10-01 14:02:35 +0530

    Update source-code.py
    my first pr

--- Applications/Voice Recorder/source-code.py
+++ Applications/Voice Recorder/source-code.py
@@ -1,7 +1,7 @@
 import sounddevice
 from scipy.io.wavfile import write
 fs=44100 #sample_rate
-second=int(input("Enter the time duration in second: ")) #enter your required time..
+second=int(input("Enter the time duration in second: ")) #enter your required time...
 print("Recording....\n")
 record_voice=sounddevice.rec(int(second * fs),samplerate=fs,channels=2)
 sounddevice.wait()
